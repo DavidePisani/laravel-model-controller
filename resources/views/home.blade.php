6ce93b74eab6dfe0ba0afcf255f60d493fb7fdca
DavidePisani <davide.pisani96@gmail.com>
2022-08-27 17:37:34 +0200

    aggiunta css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layout.app')
     <h1>Lista Film da vedere</h1>
 
+
+<div class="container">
     @foreach ($movies as $movie)
     <div class="card">
         <h2>{{$movie->title}}</h2>
         <span>{{$movie->original_title}}</span>
         <div>{{$movie->nationality}}</div>
-        <div>{{$movie->date}}</div>
-        <div>{{$movie->vote}}</div>
+        <div>Release date: {{$movie->date}}</div>
+        <div>Vote: {{$movie->vote}}</div>
     </div>
     <br>
     @endforeach
+
+</div>
+  
     
-</body>
-</html>
